fbe57154e9cbd12e7e2f022af22a824b71f8dd28
dhruv-accenture <dhruv.sagpariya@accenture.com>
2025-11-16 16:23:22 -0500

    fix: insight block

--- blocks/insights/insights.js
+++ blocks/insights/insights.js
@@ -7,7 +7,9 @@ export default function decorate(block) {
   rows.forEach((row) => {
     const cells = [...row.children];
     cells.forEach((cell) => {
-      if (cell.querySelector('h1, h2, h3, h4, h5, h6')) {
+      if (cell.classList.contains('badge')) {
+        cell.classList.add('insights-badge');
+      } else if (cell.querySelector('h1, h2, h3, h4, h5, h6')) {
         cell.classList.add('insights-heading');
       } else if (cell.querySelector('a')) {
         cell.classList.add('insights-button');
@@ -17,4 +19,5 @@ export default function decorate(block) {
     });
   });
 }
+
 
