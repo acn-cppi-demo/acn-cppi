d0ebdf121424e081f5c67b02e5d92507b139a755
dhruv-accenture <dhruv.sagpariya@accenture.com>
2025-11-18 11:15:24 -0500

    revert(cards): revert to DOM approach, combine linkLabel and link with icon
    Revert to original DOM manipulation pattern. Combine linkLabel and link elements into single link with SVG icon using template literals.

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -19,6 +19,47 @@ export default function decorate(block) {
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
+
+  // Combine linkLabel and link into a single link with icon
+  ul.querySelectorAll('li').forEach((li) => {
+    const linkLabelElement = li.querySelector('[data-aue-prop="linkLabel"]');
+    const linkElement = li.querySelector('a[href]');
+    const linkLabelBody = linkLabelElement?.closest('.cards-card-body');
+    const linkBody = linkElement?.closest('.cards-card-body');
+
+    if (linkLabelElement && linkElement) {
+      const linkLabelText = linkLabelElement.textContent.trim();
+      const linkHref = linkElement.getAttribute('href');
+      const linkTitle = linkElement.getAttribute('title') || linkHref;
+
+      // Create combined link HTML using template literal
+      const linkHtml = `
+        <p>
+          <a href="${linkHref}" 
+             title="${linkTitle}" 
+             class="cards-link">
+            ${linkLabelText}
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+              <path d="M12.175 9H0V7H12.175L6.575 1.4L8 0L16 8L8 16L6.575 14.6L12.175 9Z" fill="#0273CF"/>
+            </svg>
+          </a>
+        </p>
+      `;
+
+      // Replace linkLabel body with new combined link
+      if (linkLabelBody) {
+        linkLabelBody.innerHTML = linkHtml;
+        // Remove the separate link body if it exists
+        if (linkBody && linkBody !== linkLabelBody) {
+          linkBody.remove();
+        }
+      } else if (linkBody) {
+        // If no linkLabel body, just update the link body
+        linkBody.innerHTML = linkHtml;
+      }
+    }
+  });
+
   block.textContent = '';
   block.append(ul);
 }
